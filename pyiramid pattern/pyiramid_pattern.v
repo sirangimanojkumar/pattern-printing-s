3c451a076cbbc9c8cb88ff00797b26d7eda75be8
Sirangi Manoj Kumar <manoj49kumar818@gmail.com>
2026-06-28 01:34:58 +0530

    Enhance pyramid pattern code with comments
    Added comments to clarify the purpose of each loop.

--- pyiramid pattern/pyiramid_pattern.v
+++ pyiramid pattern/pyiramid_pattern.v
@@ -4,10 +4,11 @@ module pattern ;
   
   initial begin 
     for (i = 1;i<=5;i=i+1)begin 
+      // print leading spaces to center the row
       for(j=1;j<=5-i;j=j+1)begin
         $write(" ");
       end
-    
+     // print stars for this row
       for(k=1;k<=2*i-1;k=k+1)begin
         $write("*");
     end
